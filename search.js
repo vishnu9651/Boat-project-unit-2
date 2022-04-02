dc9af8f5ab1b62b39b05f9c77a7b4099270f2125
lalitarana123456 <lalitakumari39840@gmail.com>
2022-04-02 20:29:41 +0530

    welldone

---
+++ search.js
@@ -0,0 +1,179 @@
+var productData = [
+    { 
+     image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/viper-green_720x.png?v=1642405569" ,
+     off: " you save 56%"  ,
+     
+     rating: " 5 star " , 
+     name: "boAt Airdopes 131 - Wireless Earbuds",
+     price: 1299.00,
+     stkPrice:2999.00 ,
+     category: "wireless earbuds" ,
+     btn1 : "btn1"
+    
+    },
+    { 
+     image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main-img-R333_360x.png?v=1641801662" ,
+     off: " you save 64%"  ,
+     rating: " 5 star " , 
+     name: "boAt Rockerz 333",
+     price: 1399.00,
+     stkPrice:2999.00 ,
+     category: " wireless earphones" ,
+     btn2 : "btn2"
+    },
+    { 
+     image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main2_529ecf13-c993-4fe7-a2d6-9357f1a47db4_360x.png?v=1645164257" ,
+     off: " you save 64%"  ,
+     rating: " 5 star " , 
+     name: "boAt Watch Xtend‌",
+     price: 2799.00,
+     stkPrice:7999.00 ,
+     category: " smart watchs" ,
+     btn : "btn3"
+    },
+    
+    
+    { 
+     image: " https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main2_541e4150-6924-467d-b193-1793daa2e283_360x.png?v=1648119560" ,
+     off: "  NEW-ARRIVAL"  ,
+     rating: " 5 star " , 
+     name: "boAt  Deuce 500",
+     price: 499.00,
+     stkPrice:899.00 ,
+     category: "cable" ,
+     btn : "btn4"
+    },
+    
+    { 
+     image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main3_9230f72d-9f86-4aec-9ae2-a7d87d332877_360x.png?v=1647426798" ,
+     off: "  NEW-ARRIVAL"  ,
+     rating: " 5 star " , 
+     name: " Airdopes 411 ANC",
+     price:  2299.00,
+     stkPrice:4990.00 ,
+     category: "wireless earbuds" ,
+     btn : "btn5"
+    },
+    { 
+     image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/0cfa4417-0213-4b49-b78e-0ae68aeb7057_420x.png?v=1625046144" ,
+     off: " you save 70%"  ,
+     rating: " 5 star " , 
+     name: "boAt Rockerz 550",
+     price:  1199.00,
+     stkPrice:4999.00 ,
+     category: "wireless headphones" ,
+     btn : "btn6"
+    },
+    
+    
+    { 
+     image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/mainblack_360x.png?v=1640237575",
+     off: " you save 62%"  ,
+     rating: " 5 star " , 
+     name: "boAt watch Iris",
+     price:  4499.00,
+     stkPrice:11999.00 ,
+     category: "smart watchs" ,
+     btn : "btn7"
+    },
+    { 
+     image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/e7910a39-186a-41f6-ac57-fc3e84108104_360x.png?v=1625045106" ,
+     off: " you save 58%"  ,
+     rating: " 5 star " , 
+     name: "boAt Storm",
+     price:  2499.00,
+     stkPrice:5999.00 ,
+     category: "smart watchs" ,
+     btn : "btn8"
+    },
+    { 
+     image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/Black_2_360x.png?v=1639486284",
+     off: " you save 61%"  ,
+     rating: " 5 star " , 
+     name: "boAt watch Mystiq",
+     price:  3499.00,
+     stkPrice:8990.00 ,
+     category: "smart watchs" ,
+     btn : "btn9"
+    },
+    
+    { 
+     image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/35ca6350-d6f3-4bc2-a742-01b2c88d80c4_360x.png?v=1625552657" ,
+     off: " you save 62%"  ,
+     rating: " 5 star " , 
+     name: "boAt Rockerz 255 Pro+",
+     price: 1499.00,
+     stkPrice:3999.00 ,
+     category: "wireless earphones" ,
+     btn : "btn10"
+    },
+    
+    { 
+     image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/eb8e0fbd-c412-48b3-9c91-5b49ddf35800_360x.png?v=1625045744" ,
+     off: " you save 69%"  ,
+     rating: " 5 star " , 
+     name: "boAt Rockerz 450",
+     price:  1199.00,
+     stkPrice:3999.00 ,
+     category: "wireless headphones" ,
+     btn : "btn11"
+    },
+   
+    
+    { 
+     image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/5f7d33ad-39a9-4426-836e-c300814839b9_360x.png?v=1625046105 ",
+     off: " you save 66%"  ,
+     rating: " 5 star " , 
+     name: "boAt Rockerz 235 V2",
+     price:  999.00,
+     stkPrice:2999.00 ,
+     category: "wireless earphones" ,
+     btn : "btn12"
+    },
+    
+    { 
+     image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/553af994-244a-4b81-9d9e-9967a1b966b3_360x.png?v=1625046259" ,
+     off: " you save 60%"  ,
+     rating: " 5 star " , 
+     name: "boAt BassHeads",
+     price:  399.00,
+     stkPrice:999.00 ,
+     category: "wired earphones" ,
+     btn : "btn13"
+    },
+     
+    { 
+     image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/103_360x.png?v=1574927262" ,
+     off: " you save 70%"  ,
+     rating: " 5 star " , 
+     name: "boAt BassHead 103",
+     price:  379.00,
+     stkPrice:1290.00 ,
+     category: "wired earphones" ,
+     btn : "btn14"
+    },
+     
+    { 
+     image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/8ec58553-3255-48ec-929e-4838215b640c_360x.png?v=1625046271" ,
+     off: " you save 67%"  ,
+     rating: " 5 star " , 
+     name: "boAt BassHeads 900",
+     price:  799.00,
+     stkPrice:2499.00 ,
+     category: "wired headphones" ,
+     btn : "btn15"
+    },
+   
+    { 
+     image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main_1_e171664a-1254-45e6-9dde-2ac4f0a815c9_360x.png?v=1648466727" ,
+     off: "  NEW-ARRIVAL"  ,
+     rating: " 5 star " , 
+     name: "boAt Rockerz 235 Pro",
+     price:  999.00,
+     stkPrice:1999.00 ,
+     category: "wireless earphones" ,
+     btn : "btn100"
+    },
+    
+    ]
+    localStorage.setItem("productdetails",JSON.stringify(productData))
