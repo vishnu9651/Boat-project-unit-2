11924e0b49fe1db609d6d4a07abe8fcae1edaf08
Aifanul Islam <sufyanislam00@gmail.com>
2022-08-18 08:27:18 +0530

    first commit

---
+++ JS files/home.js
@@ -0,0 +1,326 @@
+var productData = [
+ { 
+  image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/viper-green_720x.png?v=1642405569" ,
+  off: " you save 56%"  ,
+  
+  rating: " 5 star " , 
+  name: "boAt Airdopes 131 - Wireless Earbuds",
+  price: 1299.00,
+  stkPrice:2999.00 ,
+  category: "wireless earbuds" ,
+  btn1 : "btn1"
+ 
+ },
+ { 
+  image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main-img-R333_360x.png?v=1641801662" ,
+  off: " you save 64%"  ,
+  rating: " 5 star " , 
+  name: "boAt Rockerz 333",
+  price: 1399.00,
+  stkPrice:2999.00 ,
+  category: " wireless earphones" ,
+  btn2 : "btn2"
+ },
+ { 
+  image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main2_529ecf13-c993-4fe7-a2d6-9357f1a47db4_360x.png?v=1645164257" ,
+  off: " you save 64%"  ,
+  rating: " 5 star " , 
+  name: "boAt Watch Xtend‌",
+  price: 2799.00,
+  stkPrice:7999.00 ,
+  category: " smart watchs" ,
+  btn : "btn3"
+ },
+ 
+ 
+ { 
+  image: " https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main2_541e4150-6924-467d-b193-1793daa2e283_360x.png?v=1648119560" ,
+  off: "  NEW-ARRIVAL"  ,
+  rating: " 5 star " , 
+  name: "boAt  Deuce 500",
+  price: 499.00,
+  stkPrice:899.00 ,
+  category: "cable" ,
+  btn : "btn4"
+ },
+ 
+ { 
+  image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main3_9230f72d-9f86-4aec-9ae2-a7d87d332877_360x.png?v=1647426798" ,
+  off: "  NEW-ARRIVAL"  ,
+  rating: " 5 star " , 
+  name: " Airdopes 411 ANC",
+  price:  2299.00,
+  stkPrice:4990.00 ,
+  category: "wireless earbuds" ,
+  btn : "btn5"
+ },
+ { 
+  image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/0cfa4417-0213-4b49-b78e-0ae68aeb7057_420x.png?v=1625046144" ,
+  off: " you save 70%"  ,
+  rating: " 5 star " , 
+  name: "boAt Rockerz 550",
+  price:  1199.00,
+  stkPrice:4999.00 ,
+  category: "wireless headphones" ,
+  btn : "btn6"
+ },
+ 
+ 
+ { 
+  image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/mainblack_360x.png?v=1640237575",
+  off: " you save 62%"  ,
+  rating: " 5 star " , 
+  name: "boAt watch Iris",
+  price:  4499.00,
+  stkPrice:11999.00 ,
+  category: "smart watchs" ,
+  btn : "btn7"
+ },
+ { 
+  image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/e7910a39-186a-41f6-ac57-fc3e84108104_360x.png?v=1625045106" ,
+  off: " you save 58%"  ,
+  rating: " 5 star " , 
+  name: "boAt Storm",
+  price:  2499.00,
+  stkPrice:5999.00 ,
+  category: "smart watchs" ,
+  btn : "btn8"
+ },
+ { 
+  image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/Black_2_360x.png?v=1639486284",
+  off: " you save 61%"  ,
+  rating: " 5 star " , 
+  name: "boAt watch Mystiq",
+  price:  3499.00,
+  stkPrice:8990.00 ,
+  category: "smart watchs" ,
+  btn : "btn9"
+ },
+ 
+ { 
+  image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/35ca6350-d6f3-4bc2-a742-01b2c88d80c4_360x.png?v=1625552657" ,
+  off: " you save 62%"  ,
+  rating: " 5 star " , 
+  name: "boAt Rockerz 255 Pro+",
+  price: 1499.00,
+  stkPrice:3999.00 ,
+  category: "wireless earphones" ,
+  btn : "btn10"
+ },
+ 
+ { 
+  image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/eb8e0fbd-c412-48b3-9c91-5b49ddf35800_360x.png?v=1625045744" ,
+  off: " you save 69%"  ,
+  rating: " 5 star " , 
+  name: "boAt Rockerz 450",
+  price:  1199.00,
+  stkPrice:3999.00 ,
+  category: "wireless headphones" ,
+  btn : "btn11"
+ },
+
+ 
+ { 
+  image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/5f7d33ad-39a9-4426-836e-c300814839b9_360x.png?v=1625046105 ",
+  off: " you save 66%"  ,
+  rating: " 5 star " , 
+  name: "boAt Rockerz 235 V2",
+  price:  999.00,
+  stkPrice:2999.00 ,
+  category: "wireless earphones" ,
+  btn : "btn12"
+ },
+ 
+ { 
+  image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/553af994-244a-4b81-9d9e-9967a1b966b3_360x.png?v=1625046259" ,
+  off: " you save 60%"  ,
+  rating: " 5 star " , 
+  name: "boAt BassHeads",
+  price:  399.00,
+  stkPrice:999.00 ,
+  category: "wired earphones" ,
+  btn : "btn13"
+ },
+  
+ { 
+  image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/103_360x.png?v=1574927262" ,
+  off: " you save 70%"  ,
+  rating: " 5 star " , 
+  name: "boAt BassHead 103",
+  price:  379.00,
+  stkPrice:1290.00 ,
+  category: "wired earphones" ,
+  btn : "btn14"
+ },
+  
+ { 
+  image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/8ec58553-3255-48ec-929e-4838215b640c_360x.png?v=1625046271" ,
+  off: " you save 67%"  ,
+  rating: " 5 star " , 
+  name: "boAt BassHeads 900",
+  price:  799.00,
+  stkPrice:2499.00 ,
+  category: "wired headphones" ,
+  btn : "btn15"
+ },
+
+ { 
+  image: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main_1_e171664a-1254-45e6-9dde-2ac4f0a815c9_360x.png?v=1648466727" ,
+  off: "  NEW-ARRIVAL"  ,
+  rating: " 5 star " , 
+  name: "boAt Rockerz 235 Pro",
+  price:  999.00,
+  stkPrice:1999.00 ,
+  category: "wireless earphones" ,
+  btn : "btn100"
+ },
+ 
+ ]
+ 
+  var cartArr = JSON.parse(localStorage.getItem("cart")) || []
+  
+  document.getElementById("btn1").addEventListener("click" , btn1)
+
+  function btn1(){
+   event.preventDefault();
+   cartArr.push( productData[0])
+   // console.log(cartArr)
+   localStorage.setItem("cart" , JSON.stringify(cartArr))
+   window.location.reload()
+  }
+  document.getElementById("btn2").addEventListener("click" , btn2)
+
+  function btn2(){
+   event.preventDefault();
+   cartArr.push( productData[1])
+   // console.log(cartArr)
+   localStorage.setItem("cart" , JSON.stringify(cartArr))
+   window.location.reload()
+  }
+  document.getElementById("btn3").addEventListener("click" , btn3)
+
+  function btn3(){
+   event.preventDefault();
+   cartArr.push( productData[2])
+   // console.log(cartArr)
+   localStorage.setItem("cart" , JSON.stringify(cartArr))
+   window.location.reload()
+  }
+  document.getElementById("btn4").addEventListener("click" , btn4)
+
+  function btn4(){
+   event.preventDefault();
+   cartArr.push( productData[3])
+   // console.log(cartArr)
+   localStorage.setItem("cart" , JSON.stringify(cartArr))
+   window.location.reload()
+  }
+  document.getElementById("btn5").addEventListener("click" , btn5)
+
+  function btn5(){
+   event.preventDefault();
+   cartArr.push( productData[4])
+   // console.log(cartArr)
+   localStorage.setItem("cart" , JSON.stringify(cartArr))
+   window.location.reload()
+  }
+  document.getElementById("btn6").addEventListener("click" , btn6)
+
+  function btn6(){
+   event.preventDefault();
+   cartArr.push( productData[5])
+   // console.log(cartArr)
+   localStorage.setItem("cart" , JSON.stringify(cartArr))
+   window.location.reload()
+  }
+  document.getElementById("btn7").addEventListener("click" , btn7)
+
+  function btn7(){
+   event.preventDefault();
+   cartArr.push( productData[6])
+   // console.log(cartArr)
+   localStorage.setItem("cart" , JSON.stringify(cartArr))
+   window.location.reload()
+  }
+  document.getElementById("btn8").addEventListener("click" , btn8)
+
+  function btn8(){
+   event.preventDefault();
+   cartArr.push( productData[7])
+   // console.log(cartArr)
+   localStorage.setItem("cart" , JSON.stringify(cartArr))
+   window.location.reload()
+  }
+  document.getElementById("btn9").addEventListener("click" , btn9)
+
+  function btn9(){
+   event.preventDefault();
+   cartArr.push( productData[8])
+   // console.log(cartArr)
+   localStorage.setItem("cart" , JSON.stringify(cartArr))
+   window.location.reload()
+  }
+  document.getElementById("btn10").addEventListener("click" , btn10)
+
+  function btn10(){
+   event.preventDefault();
+   cartArr.push( productData[9])
+   // console.log(cartArr)
+   localStorage.setItem("cart" , JSON.stringify(cartArr))
+   window.location.reload()
+  }
+  document.getElementById("btn11").addEventListener("click" , btn11)
+
+  function btn11(){
+   event.preventDefault();
+   cartArr.push( productData[10])
+   // console.log(cartArr)
+   localStorage.setItem("cart" , JSON.stringify(cartArr))
+   window.location.reload()
+  }
+  document.getElementById("btn12").addEventListener("click" , btn12)
+
+  function btn12(){
+   event.preventDefault();
+   cartArr.push( productData[11])
+   // console.log(cartArr)
+   localStorage.setItem("cart" , JSON.stringify(cartArr))
+   window.location.reload()
+  }
+  document.getElementById("btn13").addEventListener("click" , btn13)
+
+  function btn13(){
+   event.preventDefault();
+   cartArr.push( productData[12])
+   // console.log(cartArr)
+   localStorage.setItem("cart" , JSON.stringify(cartArr))
+   window.location.reload()
+  }
+  document.getElementById("btn14").addEventListener("click" , btn14)
+
+  function btn14(){
+   event.preventDefault();
+   cartArr.push( productData[13])
+   // console.log(cartArr)
+   localStorage.setItem("cart" , JSON.stringify(cartArr))
+   window.location.reload()
+  }
+  document.getElementById("btn15").addEventListener("click" , btn15)
+
+  function btn15(){
+   event.preventDefault();
+   cartArr.push( productData[14])
+   // console.log(cartArr)
+   localStorage.setItem("cart" , JSON.stringify(cartArr))
+   window.location.reload()
+  }
+  
+  // console.log(cartArr)
+    
+  
+var addCartArr = JSON.parse(localStorage.getItem("cart"))
+
+console.log(addCartArr)
+var len = addCartArr.length
+console.log(len)
+document.getElementById("cartItems").innerText = len;
